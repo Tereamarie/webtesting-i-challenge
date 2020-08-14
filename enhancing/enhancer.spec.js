5ad6cb3b8c532491c3c5bd9cc2baee0125eeb321
marie Hotchkiss <mtknowlton67@gmail.com>
2020-08-13 19:23:16 -0700

    MVP

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,110 +1,102 @@
-const { repair, success, fail }  = require('./enhancer.js');
+const enhancer = require('./enhancer.js');
 // test away!
-describe('enhance item functions', () => {
-  describe('repair()', () => {
-    it('takes in an item and changes durability to 100', () => {
-      const item = {
-        name: 'Excallibur',
-        durability: 10,
-        enhancement: 15,
-      };
 
-      const expected = {
-        name: 'Excallibur',
-        durability: 100,
-        enhancement: 15,
-      };
-
-      const repairedItem = repair(item);
-
-      expect(repairedItem).toEqual(expected);
+describe("repair", () => {
+    const item = {
+      name: "Item 1",
+      enhancement: 10,
+      durability: 35,
+    };
+  
+    it("can repair an items durability to 100", () => {
+      const expectedOutput = 100;
+      const actualOutput = enhancer.repair(item);
+      expect(actualOutput.durability).toBe(expectedOutput);
     });
-  });
+});
 
-  describe('success()', () => {
-    it('should raise enhancement by 1', () => {
-      const item = {
-        name: 'Excallibur',
-        durability: 10,
-        enhancement: 15,
-      };
-      const expected = {
-        name: 'Excallibur',
-        durability: 10,
-        enhancement: 16,
-      };
-
-      const enhancedItem = success(item);
-      expect(enhancedItem).toEqual(expected);
+describe("success", () => {
+    const item = {
+      name: "Item 1",
+      enhancement: 10,
+      durability: 35,
+    };
+  
+    it("can enhance an item by 1", () => {
+      const expectedOutput = 11;
+      const actualOutput = enhancer.success(item);
+      expect(actualOutput.enhancement).toBe(expectedOutput);
+      expect(actualOutput.durability).toBe(35);
     });
-  });
-
-  describe('fail()', () => {
-    it('decrease durability by 5 if enhancement is less than 15', () => {
-      const item = {
-        name: 'Excallibur',
-        durability: 50,
-        enhancement: 14,
+  
+    it("will not enhance an item already at the max enhancement", () => {
+      const maxEnhancement = {
+        name: "Item 1",
+        enhancement: 20,
+        durability: 35,
       };
-
-      const expected = {
-        name: 'Excallibur',
-        durability: 45,
-        enhancement: 14,
-      };
-
-      const failItem = fail(item);
-      expect(failItem).toEqual(expected);
+  
+      const expectedOutput = 20;
+      const actualOutput = enhancer.success(maxEnhancement);
+      expect(actualOutput.enhancement).toBe(expectedOutput);
     });
+});
 
-    it('decrease durability by 10 if ehnacement is greater than 14', () => {
+describe("fail", () => {
+    it("will decrease by 5 if enhancement is less than 15", () => {
       const item = {
-        name: 'Excallibur',
-        durability: 50,
-        enhancement: 16,
+        name: "Item 1",
+        enhancement: 10,
+        durability: 35,
       };
-
-      const expected = {
-        name: 'Excallibur',
-        durability: 40,
-        enhancement: 16,
-      };
-
-      const failItem = fail(item);
-      expect(failItem).toEqual(expected);
+      const expected = 5;
+      const actual = enhancer.fail(item);
+      expect(actual.enhancement).toBe(expected);
     });
-
-    it('decrease enhancement by 1 and durability by 10 if ehnacement is greater than 16', () => {
+  
+    it("will decrease by 1 if enhancement is greater than 16", () => {
       const item = {
-        name: 'Excallibur',
-        durability: 50,
-        enhancement: 18,
-      };
-
-      const expected = {
-        name: 'Excallibur',
-        durability: 40,
+        name: "Item 1",
         enhancement: 17,
+        durability: 35,
       };
+      const expected = 16;
+      const actual = enhancer.fail(item);
+      expect(actual.enhancement).toBe(expected);
+    });
+  
+    it("will decrease by 10 if enhancement is greater than 15 and less than 16", () => {
+      const item = {
+        name: "Item 1",
+        enhancement: 16,
+        durability: 35,
+      };
+      const expected = 6;
+      const actual = enhancer.fail(item);
+      expect(actual.enhancement).toBe(expected);
+    });
+});
 
-      const failItem = fail(item);
-      expect(failItem).toEqual(expected);
+describe("get", () => {
+    it("does not mofify the name if enhancement is 0", () => {
+      const item = {
+        name: "Item 1",
+        enhancement: 0,
+        durability: 35,
+      };
+      const expected = "Item 1";
+      const actual = enhancer.get(item);
+      expect(actual.name).toBe(expected);
     });
-    it('fail test to see if decrease enhancement by 1 and durability by 10 if enhancement is greater than 16', () => {
-        const item = {
-            name: 'Excallibur',
-            durability: 50,
-            enhancement: 19,
-          };
-    
-          const expected = {
-            name: 'Excallibur',
-            durability: 40,
-            enhancement: 17,
-          };
-    
-          const failItem = fail(item);
-          expect(failItem).not.toEqual(expected);
+  
+    it("modifies the name to include enhancement level if it is greater than 0", () => {
+      const item = {
+        name: "Item 1",
+        enhancement: 5,
+        durability: 35,
+      };
+      const expected = "[+5] Item 1";
+      const actual = enhancer.get(item);
+      expect(actual.name).toBe(expected);
     });
-  });
 });
